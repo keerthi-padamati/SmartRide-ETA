4be90f3361c355b977721fb85c9639d2dbbc2280
Keerthi <keerthipadamati16@gmail.com>
2026-02-03 22:01:35 +0530

    Set up city graph and ride-sharing service
    Initialize city graph and ride-sharing service with drivers and users.

--- src/RideSharingETA.java
+++ src/RideSharingETA.java
@@ -36,3 +36,51 @@ class User
 
 
 
+
+
+
+
+
+
+
+
+public class RideSharingETA
+{
+
+    public static void main(String[] args) throws InterruptedException
+    {
+
+        Map<String, List<Edge>> city = new HashMap<>();
+        
+        city.put("A", List.of(new Edge("B", 5), new Edge("C", 10)));
+        city.put("B", List.of(new Edge("A", 5), new Edge("C", 3), new Edge("D", 7)));
+        city.put("C", List.of(new Edge("A", 10), new Edge("B", 3), new Edge("D", 1)));
+        city.put("D", List.of(new Edge("B", 7), new Edge("C", 1)));
+
+        CityGraph graph = new CityGraph(city);
+        RideSharingService service = new RideSharingService(graph);
+
+        List<Driver> drivers = List.of(
+                new Driver("D1", "A"),
+                new Driver("D2", "B"),
+                new Driver("D3", "C")
+        );
+
+        List<User> users = List.of(
+                new User("U1", "D"),
+                new User("U2", "C")
+        );
+
+
+
+
+
+
+
+
+        
+    }
+}
+
+
+
